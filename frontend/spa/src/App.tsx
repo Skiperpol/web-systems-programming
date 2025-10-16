39d332f36c775c413255a747c8b581953ac5c753
Dawid <skiperpol1@gmail.com>
2025-10-16 18:01:29 +0200

    feat: update routing in frontend SPA to include product, warehouse, and discount pages

--- frontend/spa/src/App.tsx
+++ frontend/spa/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { MainLayout } from './components/layout/MainLayout';
 import { ClientsPage } from './modules/client/pages/ClientsPage';
+import { WarehousesPage } from './modules/warehouse/pages/WarehousesPage';
+import { DiscountsPage } from './modules/discount/pages/DiscountsPage';
+import { ProductsPage } from './modules/product/pages/ProductsPage';
 
 type RouteConfig = [string, React.ReactNode];
 
 const links: RouteConfig[] = [
-  ["/products", null],
-  ["/warehouses", null],
-  ["/discounts", null],
+  ["/products", <ProductsPage />],
+  ["/warehouses", <WarehousesPage />],
+  ["/discounts", <DiscountsPage />],
   ["/clients", <ClientsPage />],
   ["/discounts-view", null],
 ];
